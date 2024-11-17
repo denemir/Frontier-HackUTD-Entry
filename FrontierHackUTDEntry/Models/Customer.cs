ac0c53613d467b34136508935a63f4b7dc83a44c
Riley <riley@cnpbagwell.com>
2024-11-16 23:48:01 -0600

    Database

---
+++ FrontierHackUTDEntry/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿public class Customer
+{
+    public string AcctId { get; set; }
+    public int Extenders { get; set; }
+    public int WirelessClientsCount { get; set; }
+    public int WiredClientsCount { get; set; }
+    public double RxAvgBps { get; set; }
+    public double TxAvgBps { get; set; }
+    public double RxP95Bps { get; set; }
+    public double TxP95Bps { get; set; }
+    public double RxMaxBps { get; set; }
+    public double TxMaxBps { get; set; }
+    public double RssiMean { get; set; }
+    public double RssiMedian { get; set; }
+    public double RssiMax { get; set; }
+    public double RssiMin { get; set; }
+    public string NetworkSpeed { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public bool WholeHomeWifi { get; set; }
+    public bool WifiSecurity { get; set; }
+    public bool WifiSecurityPlus { get; set; }
+    public bool PremiumTechPro { get; set; }
+    public bool IdentityProtection { get; set; }
+    public bool FamilyIdentityProtection { get; set; }
+    public bool TotalShield { get; set; }
+    public bool YouTubeTv { get; set; }
+}
