8d0b89757d250d2ecb2f84dd258dda5fc9ea28f8
Riley <riley@cnpbagwell.com>
2024-11-16 22:41:47 -0600

    added entityframeworkcore package

--- FrontierHackUTDEntry/obj/Debug/net8.0/FrontierHackUTDEntry.AssemblyInfo.cs
+++ FrontierHackUTDEntry/obj/Debug/net8.0/FrontierHackUTDEntry.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontierHackUTDEntry")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d814b34350989ef88f6eee73d2e49762a7d5de6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f25f0c9d007f09a163e75b3881241966a330a30f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontierHackUTDEntry")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontierHackUTDEntry")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
