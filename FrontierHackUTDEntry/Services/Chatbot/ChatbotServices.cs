04fab750f6101f466ed03fb8b323f0cdb3d672af
Danny <39076480+denemir@users.noreply.github.com>
2024-11-17 08:04:19 -0600

    support page

--- FrontierHackUTDEntry/Services/Chatbot/ChatbotServices.cs
+++ FrontierHackUTDEntry/Services/Chatbot/ChatbotServices.cs
@@ -8,7 +8,8 @@ namespace FrontierHackUTDEntry.Services.Chatbot
     public class ChatbotServices : IChatbotServices
     {
         private readonly HttpClient _httpClient;
-        private readonly string _apiKey = "<api-key>";
+        private readonly string _apiKey = "0b64b29e-f81e-43ca-8c77-b81eb9079d74";
+        private readonly string prompt = "You are a Frontier Internet support chat bot. The user's network consists of the following attributes:\r\n\"acct_id, extenders, wireless_clients_count, wired_clients_count, rx_avg_bps, tx_avg_bps, rx_p95_bps, tx_p95_bps, rx_max_bps, tx_max_bps, rssi_mean, rssi_median, rssi_max, rssi_min, network_speed, city, state, whole_home_wifi, wifi_security, wifi_security_plus, premium_tech_pro, identity_protection, family_identity_protection, total_shield, youtube_tv\"\r\nHere are the values in the same order:\r\n'00019f1459bdfbdae7d3d172c4a8b5bf01955e83ea18febafd8ecb2ecaef0704', '0', '18', '1', '413874.95263157896000000000', '4854.06842105263100000000', '1161072.900000000100000', '13906.526315789473000', '1782036.600000000300000', '48182.668421052630000', '-58.40', '-60.00', '-39.00', '-82.00', '1000.0M', 'WESLEY CHAPEL', 'FL', '1', '1', '0', '0', '0', '0', '0', '0'\r\nThese are statistics about the network, or products that they have purchased.\r\nYour initial message will be as follows:\r\n\"Hi! How can I be of assistance?\r\n1. My internet is too slow.\r\n2. I'm paying too much.\r\n3. My physical hardware broke.\r\nOr feel free to ask me anything else!\"\r\nIf the user answers for option 1, use the user's network information to diagnose a solution in 2-3 sentences that is easy for a non-technical user to understand.\r\nIf the user answers for option 2, they may choose from Fiber 500, 1 gig, 2 gig, 5 gig, and 7 gig. If they aren't fully using their bandwidth, suggest saving money by downgrading instead of canceling the plan. Otherwise, it is up to you to come up with a good answer.\r\nIf the user answers for option 3, walk them through how they may fix it.\r\nIf the user doesn't answer for any of these options, you may decide what is best for the conversation. Please make sure that there are new line characters between the list of options and the sentences, as well as indenting the options to make it easier to read as well.";
 
         public ChatbotServices(HttpClient httpClient)
         {
@@ -24,7 +25,7 @@ namespace FrontierHackUTDEntry.Services.Chatbot
                 model = "Meta-Llama-3.1-8B-Instruct",
                 messages = new[]
                 {
-            new { role = "system", content = "You are a helpful assistant" },
+            new { role = "system", content = prompt },
             new { role = "user", content = userMessage }
         }
             };
--- FrontierHackUTDEntry/wwwroot/js/Support/Support.js
+++ FrontierHackUTDEntry/wwwroot/js/Support/Support.js
@@ -1,12 +1,40 @@
-﻿$('#chat').keypress(function (e) {
+﻿var botui = new BotUI('my-botui-app');
+
+$(document).ready(function () {
+    botui.message.add({
+        content: 'Hello! How can I assist you today?',
+        delay: 1000,
+        loading: true
+    });
+});
+
+$('#chat').keypress(function (e) {
     if (e.which == 13) {
+        var msg = e.currentTarget.value;
+        sendMessage(msg);
         $.ajax({
             url: '/api/Chatbot/SendMessage',
             contentType: "application/x-www-form-urlencoded",
             method: 'POST',
-            data: { userMessage: e.currentTarget.value },
+            data: { userMessage: msg },
         }).done(function (response) {
-            $('#chat').val('');
+            receiveMessage(response);
         });
     }
 });
+
+function sendMessage(input) {
+    botui.message.add({
+        content: input,
+        human: true
+    });
+    $('#chat').val('');
+}
+
+function receiveMessage(response) {
+    botui.message.add({
+        content: response,
+        delay: 1000,
+        loading: true
+    });
+}
